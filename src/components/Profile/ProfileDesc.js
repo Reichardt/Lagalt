fd0a5dd8bede0a02bbbd29566d1544ae6d089450
Valdemar Kragh <valdemar.kragh@hotmail.com>
2021-06-10 12:29:13 +0200

    FEAT: Adding design

--- src/components/Profile/ProfileDesc.js
+++ src/components/Profile/ProfileDesc.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import { useState } from 'react';
 
-function ProfileDesc() {
-	return (
-		<div className="profile-desc ps-5 py-5 text-start">
+function ProfileDesc({ profile, profileParam }) {
+	const [checked, setChecked] = useState(false);
+
+	const handleSwitchChange = () => {
+		setChecked(!checked);
+	};
+
+	const renderTextDesc = () => {
+		return (
 			<p>
 				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
 				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
@@ -12,6 +18,39 @@ function ProfileDesc() {
 				occaecat cupidatat non proident, sunt in culpa qui officia deserunt
 				mollit anim id est laborum.
 			</p>
+		);
+	};
+
+	const renderTextArea = () => {
+		return (
+			<textarea
+				class="form-control"
+				defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+	veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+	commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+	velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+	occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+	mollit anim id est laborum."
+			/>
+		);
+	};
+
+	return (
+		<div className="profile-desc ps-5 py-5 text-start">
+			{checked ? renderTextArea() : renderTextDesc()}
+			{profile && profileParam === profile.username && (
+				<div className="form-check form-switch">
+					<label>Edit description</label>
+					<input
+						className="form-check-input "
+						type="checkbox"
+						id="flexSwitchCheckChecked"
+						checked={checked}
+						onChange={handleSwitchChange}
+					/>
+				</div>
+			)}
 		</div>
 	);
 }
--- src/components/Profile/ProfileSkills.js
+++ src/components/Profile/ProfileSkills.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import { useState } from 'react';
 import ProfileSkill from './ProfileSkill';
 
-function ProfileSkills() {
+function ProfileSkills({ profile, profileParam }) {
+	const [checked, setChecked] = useState(false);
+
+	const handleSwitchChange = () => {
+		setChecked(!checked);
+	};
+
 	return (
 		<div>
-			<h2 className="text-primary">Skills</h2>
+			<div className="d-flex justify-content-between align-items-center pe-3">
+				<h2 className="text-primary">Skills</h2>
+				{profile && profileParam === profile.username && (
+					<div className="form-check form-switch">
+						<label>Edit skills</label>
+						<input
+							className="form-check-input "
+							type="checkbox"
+							id="flexSwitchCheckChecked"
+							checked={checked}
+							onChange={handleSwitchChange}
+						/>
+					</div>
+				)}
+			</div>
 			<hr />
 			<ProfileSkill skill={'Web development'} />
 			<ProfileSkill skill={'Web development'} />
