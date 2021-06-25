72ff42b18a56ce93f0e15c9bdd2f029944705a44
Mikkel Hammer <mikkel_fhammer@hotmail.com>
2021-06-25 11:58:32 +0200

    FEAT: added searching by category

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+	fetchAllProjectsByCategory,
+	projectSelector
+} from '../features/Project/projectSlice';
+import Sidenav from '../components/Sidenav/Sidenav';
+import ProjectList from '../components/ProjectList/ProjectList';
+import Loader from '../components/Global/Loader';
+
 
 function Category({ match }) {
 	const { category } = match.params;
+	const dispatch = useDispatch();
+	const { projects, loading } = useSelector(projectSelector);
+
+	useEffect(() => {
+		dispatch(fetchAllProjectsByCategory(category))
+	}, [dispatch])
+
 	return (
-		<div>
-			<p>{category}</p>
+		<div className="container">
+			<div className="row">
+				<Sidenav side={'left'} />
+				{loading && <Loader styles={loaderStyles} />}
+				{!loading && (
+					<>
+						<ProjectList projects={projects} page={`category - ${category}`} />
+						<Sidenav side={'right'} />
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
 
 export default Category;
+
+const loaderStyles = {
+	top: '50%',
+	left: '50%',
+	zIndex: '2',
+};
